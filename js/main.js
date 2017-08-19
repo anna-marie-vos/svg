41d5ab5bb7b098286c573934afe1e88f4dbfa614
anna-marie-vos <amvos@outlook.com>
2017-08-20 11:05:35 +1200

    clearing the scene

--- js/main.js
+++ js/main.js
@@ -4,18 +4,30 @@
 	'use strict';
 
 	// declare actors here
+	const $backFallingLeaves = $('#brownLeaf, #redLeaf, #orangeLeaf')
 
-	// clear stage 
+	// clear stage
+function clearStage() {
+	const clearTL = new TimelineMax()
+	clearTL
+		.set($backFallingLeaves, {autoAlpha:0} )
 
+	return clearTL
+}
 	// enter floor vegetation
 
 	// enter tree
 
 	// enter the greeting text
-	
+
 	// the GO function ...to kick things all off
-	
+	function go() {
+		console.log('go...');
+		const masterTL = new TimelineMax()
+		masterTL
+			.add(clearStage(),'scene-clear-stage')
+	}
+
+go()
 
 })(jQuery);
-
-
