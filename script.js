93fb459b53e900fe8d3c72239ae1f551e4bfbbf6
anna-marie-vos <amvos@outlook.com>
2017-08-19 20:41:00 +1200

    successfully loaded gsap

---
+++ script.js
@@ -0,0 +1,19 @@
+// logo id == '#logo1'
+
+console.log("hello")
+$(document).ready(function(){
+  animateLogo();
+})
+
+//TweenMax.to('#logo1', 1, {x:850, y:830, ease: Power4.easeInOut})
+function animateLogo() {
+  TweenMax.to('#logo1', 1, {x:850})
+}
+//TweenMax.to('#logo1', 1, {y:830, delay:1})
+
+//TweenMax.from('#logo1', 1, {x:850})
+
+// TweenMax.fromTo('#logo1', 1,
+//   {autoAlpha:1, x:0,y:830},
+//   {autoAlpha:0.2,x:850, y:830}
+// )
