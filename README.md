73aae8103ceaa5deae8446d0f6bb92697f5263b8
anna-marie-vos <amvos@outlook.com>
2017-08-19 21:34:02 +1200

    gsap animations works

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # svg
+* https://greensock.com/
+* this shows the ease paths: https://greensock.com/ease-visualizer
--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 // logo id == '#logo1'
-
-console.log("hello")
 $(document).ready(function(){
-  animateLogo();
+  animateAllLogos();
 })
 
-//TweenMax.to('#logo1', 1, {x:850, y:830, ease: Power4.easeInOut})
 function animateLogo() {
-  TweenMax.to('#logo1', 1, {x:850})
+  // TweenMax.to('#logo1', 1, {x:850, y:830, ease: Power4.easeInOut} )
+
+  // TweenMax.to('#logo1', 1, {x:850})
+  // TweenMax.to('#logo1', 1, {y:830, delay:1})
+
+  // TweenMax.from('#logo1', 1, {x:850})
+
+  TweenMax.fromTo('#logo1', 1,
+    {autoAlpha:1, x:0,y:830},
+    {autoAlpha:0.2,x:850, y:830}
+  )
 }
-//TweenMax.to('#logo1', 1, {y:830, delay:1})
 
-//TweenMax.from('#logo1', 1, {x:850})
+function animateAllLogos() {
+    // TweenMax.to(['#logo1','#logo2','#logo3'], 1, {y:830})
+    // TweenMax.staggerTo(['#logo1','#logo2','#logo3'], 1, {y:830}, 0.25)
+    // TweenMax.staggerFrom(['#logo1','#logo2','#logo3'], 1, {y:830}, 0.25)
+    TweenMax.staggerFromTo(['#logo1','#logo2','#logo3'], 1, {y:300}, {y:830, ease:Bounce.easeOut }, 0.25)
 
-// TweenMax.fromTo('#logo1', 1,
-//   {autoAlpha:1, x:0,y:830},
-//   {autoAlpha:0.2,x:850, y:830}
-// )
+}
