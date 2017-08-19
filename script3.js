37c32d474f717727b38bbacd08b2b80967d4d008
anna-marie-vos <amvos@outlook.com>
2017-08-20 09:41:39 +1200

    timelines

--- script3.js
+++ script3.js
@@ -1,6 +1,6 @@
 // logo id == '#logo1'
 $(document).ready(function(){
-
+  timelines()
 })
 
 function animateLogo() {
@@ -22,4 +22,18 @@ function animateAllLogos() {
     // TweenMax.staggerTo(['#logo1','#logo2','#logo3'], 1, {y:830}, 0.25)
     // TweenMax.staggerFrom(['#logo1','#logo2','#logo3'], 1, {y:830}, 0.25)
     TweenMax.staggerFromTo(['#logo1','#logo2','#logo3'], 1, {y:300}, {y:830, ease:Bounce.easeOut }, 0.25)
+}
+
+function timelines() {
+  const masterTl = new TimelineMax()
+  masterTl.to('#logo1', 1, {x:850})
+          .to('#logo2', 1, {y:830},'-=0.5') //starting animation earlier
+          .to('#logo3', 1, {x:'-=850'}, '-=0.5') //this is a relative reference
+          .add(slavetimeline(),'-=0.5')
+}
+
+function slavetimeline() {
+  const logo3Tl = new TimelineMax()
+  logo3Tl.to('#logo3', 1, {y:'-=830'})
+  return logo3Tl
 }
