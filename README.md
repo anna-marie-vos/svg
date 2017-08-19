ef1b6f36e8d499e22a4869bf1f2bd4e1a1dced44
anna-marie-vos <amvos@outlook.com>
2017-08-20 10:12:34 +1200

    callback added

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 * Cool example: http://alfredservice.com/
 * https://greensock.com/
 * this shows the ease paths: https://greensock.com/ease-visualizer
+
+* callback: https://greensock.com/docs/TweenMax/eventCallback()
--- script4.js
+++ script4.js
@@ -6,16 +6,16 @@ $(document).ready(function(){
 function MasterTime() {
   const masterTl = new TimelineMax()
   masterTl
+    .add('beggining') //this is a label
     .to('#logo1', 1, {x:850})
-    .to('#logo2', 1, {y:830},'-=0.5')
-    .to('#logo3', 1, {y:'-=850'},'-=0.5')
+    .add('endLogo1')
+    .to('#logo2', 1, {y:830 },'endLogo1-=0.5')
+    .to('#logo3', 1, {x:'-=850', onComplete: showBigLogo},'-=0.5') //onCompleteis a callback
 }
 
 function showBigLogo(color) {
-  var bigL = document.querySelector('#bigLogo');
-
+  const bigL = document.querySelector('#bigLogo')
   TweenMax.set(bigL, {
-    autoAlpha: 1,
-    fill: color
-  });
+    autoAlpha:1
+  })
 }
