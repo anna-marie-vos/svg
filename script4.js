821ce625aa979e8efd3275a520d8cd4cd6d698ee
anna-marie-vos <amvos@outlook.com>
2017-08-20 10:31:13 +1200

    finished

--- script4.js
+++ script4.js
@@ -10,12 +10,13 @@ function MasterTime() {
     .to('#logo1', 1, {x:850})
     .add('endLogo1')
     .to('#logo2', 1, {y:830 },'endLogo1-=0.5')
-    .to('#logo3', 1, {x:'-=850', onComplete: showBigLogo},'-=0.5') //onCompleteis a callback
+    .to('#logo3', 1, {x:'-=850', onComplete: showBigLogo, onCompleteParams:['orange']},'-=0.5') //onCompleteis a callback
 }
 
 function showBigLogo(color) {
   const bigL = document.querySelector('#bigLogo')
   TweenMax.set(bigL, {
-    autoAlpha:1
+    autoAlpha:1,
+    fill:color
   })
 }
